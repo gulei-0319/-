f0f2fac0966307a29f1cb03f825c30376fe0b840
gulei-0319 <15801305279@163.com>
2019-09-21 19:13:23 +0800

    第一天练习

---
+++ test.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+'''
+a=5
+b=2.5
+c='hello'
+print(type(a))
+print(type(b))
+print(type(c))
+'''
+#a,b=input('please input a value :').split(',')
+#print('The value of a is :',a,b,type(a),type(b))
+
+#s=[11,13,5,7,0,56,23,34,72]
+
+#print (max(s))
+#print(min(s))
+#print (len(s))
+#print s.index(5)
+#print s.append(7)
+#print del s[4],
+
+a=int (input('输入分数))
+if 90<a<100
+    a=A
+    elif
